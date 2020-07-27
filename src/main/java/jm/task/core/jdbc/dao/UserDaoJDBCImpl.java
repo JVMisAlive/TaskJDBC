773173949ce6a1cab6af4d383e946116623ece70
JVMisAlive <megamen1067!>
2020-07-28 00:53:33 +0300

    CoreTaskDB
    Signed-off-by: JVMisAlive <Megamen1067!>

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,95 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+    private Connection connection = null;
+    private Statement statement = null;
+    private final List<User> list = new ArrayList<>();
+
+
     public UserDaoJDBCImpl() {
+        try {
+            connection = Util.getConnection();
+        } catch (SQLException | ClassNotFoundException exception) {
+            exception.printStackTrace();
+        }
 
     }
 
     public void createUsersTable() {
-
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate("CREATE TABLE USERS " +
+                    "(id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    " name VARCHAR(32) null, " +
+                    " last_name VARCHAR(32) null, " +
+                    " age TINYINT)");
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try {
+            statement = connection.createStatement();
+            statement.executeUpdate(
+                    "DROP TABLE IF EXISTS USERS");
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (PreparedStatement ps = connection.prepareStatement(
+                "INSERT INTO USERS (name, last_name, age) VALUES (?, ?, ?)")) {
+            ps.setString(1, name);
+            ps.setString(2, lastName);
+            ps.setByte(3, age);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (PreparedStatement ps = connection.prepareStatement(
+                "DELETE FROM USERS WHERE id = " + id)) {
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+
+        try {
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT * FROM USERS");
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("name");
+                String lastName = resultSet.getString("last_name");
+                byte age = resultSet.getByte("age");
+                list.add(new User(name, lastName, age));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return list;
+
     }
 
     public void cleanUsersTable() {
-
+        try {
+            statement.executeUpdate(
+                    "DELETE FROM USERS");
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,14 @@
 package jm.task.core.jdbc.util;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/coretaskdb?USER=root&password=" +
+                "Root1067!&useLegacyDatetimeCode=false&serverTimezone=Europe/Moscow");
+    }
 }
